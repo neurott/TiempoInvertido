463dca9c3da5a5f2f1d8ad5644e440a56461c45b
neurott <neurott@protonmail.com>
2026-09-18 19:39:48 -0300

    MovieService y ShowService creados, faltan los otros

--- backend/src/main/java/cl/tiempoInvertido/backend/service/MovieService.java
+++ backend/src/main/java/cl/tiempoInvertido/backend/service/MovieService.java
@@ -1,4 +1,32 @@
 package cl.tiempoInvertido.backend.service;
 
+import cl.tiempoInvertido.backend.model.Movie;
+import cl.tiempoInvertido.backend.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MovieService {
+
+    private final MovieRepository movieRepo;
+
+    public List<Movie> findAll(){
+        return movieRepo.findAll();
+    }
+
+    public Movie findById(Long id){
+        return movieRepo.findById(id).orElseThrow(() -> new RuntimeException("Movie not found: " + id));
+    }
+
+    public Movie save(Movie movie){
+        return movieRepo.save(movie);
+    }
+
+    public void deleteById(Long id){
+        movieRepo.deleteById(id);
+    }
+
 }
---
+++ backend/src/main/java/cl/tiempoInvertido/backend/service/ShowService.java
@@ -0,0 +1,33 @@
+package cl.tiempoInvertido.backend.service;
+
+
+import cl.tiempoInvertido.backend.model.Show;
+import cl.tiempoInvertido.backend.repository.ShowRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ShowService {
+
+    private final ShowRepository showRepo;
+
+
+    public List<Show> findAll(){
+        return showRepo.findAll();
+    }
+
+    public Show findById(Long id){
+        return showRepo.findById(id).orElseThrow(() -> new RuntimeException("Show not found: " + id));
+    }
+
+    public Show save(Show show){
+        return showRepo.save(show);
+    }
+
+    public void deleteById(Long id){
+        showRepo.deleteById(id);
+    }
+}
