3300fe5ae8988b59070dc85a687c17b3365cede1
neurott <neurott@protonmail.com>
2026-09-22 15:10:21 -0300

    algo del repo

--- backend/src/main/java/cl/tiempoInvertido/backend/repository/EpisodeRepository.java
+++ backend/src/main/java/cl/tiempoInvertido/backend/repository/EpisodeRepository.java
@@ -4,6 +4,10 @@ import cl.tiempoInvertido.backend.model.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+
+    List<Episode> findByShowId(Long showId);
 }
